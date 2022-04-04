0969d796080278c6eac98d1062206b425ad35692
NikarashiHatsu <yourlovelydev@gmail.com>
2022-04-04 17:47:55 +0700

    chore: inertia initialisation

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,16 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .react()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
