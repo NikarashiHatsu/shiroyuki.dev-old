96eac9f60de5fe3663f67827ba12979365b80482
NikarashiHatsu <yourlovelydev@gmail.com>
2022-04-08 17:30:30 +0700

    chore: dashboard layout

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -1,123 +1,137 @@
 import React, { useState } from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Link } from '@inertiajs/inertia-react';
+import { Head, Link } from '@inertiajs/inertia-react';
+import Logo from './../../images/devsnote.svg';
+import { SignOut, House, Gauge, Gear, Note, Tag, ChatTeardropText, Users, List } from 'phosphor-react';
 
-export default function Authenticated({ auth, header, children }) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-
+export default function Authenticated({ auth, children }) {
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex justify-between h-16">
-                        <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto text-gray-500" />
-                                </Link>
-                            </div>
+        <>
+            <Head>
+                <meta name='description' content="Developer's Note Dashboard" />
+                <link rel="shortcut icon" href={Logo} type="image/x-icon" />
+            </Head>
 
-                            <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
-                                </NavLink>
-                            </div>
+            <div className='drawer drawer-mobile' data-theme="light">
+                <input id="drawer" type="checkbox" className='drawer-toggle' />
+                <div className='drawer-content'>
+                    <div className='navbar bg-base-100'>
+                        <div className='flex-none flex md:hidden'>
+                            <label htmlFor="drawer" className='btn btn-square btn-ghost'>
+                                <List size={16} />
+                            </label>
                         </div>
+                    </div>
 
-                        <div className="hidden sm:flex sm:items-center sm:ml-6">
-                            <div className="ml-3 relative">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {auth.user.name}
-
-                                                <svg
-                                                    className="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <div className="-mr-2 flex items-center sm:hidden">
-                            <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                    <div className='p-6'>
+                        {children}
+                    </div>
+                </div>
+                <div className='drawer-side border-r'>
+                    <label htmlFor="drawer" className='drawer-overlay'></label>
+                    <div className='overflow-y-auto w-80 bg-white'>
+                        <div className='border-b sticky top-0 border-r px-3 py-3 z-50 backdrop-blur'>
+                            <Link
+                                className='flex items-center'
+                                href={ route('dashboard') }
                             >
-                                <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
+                                <img src={Logo} className='w-12 h-12' />
+                                <div className='ml-2 flex flex-col'>
+                                    <span className='text-sm'>
+                                        Shiroyuki.dev
+                                    </span>
+                                    <span className='text-xs'>
+                                        A Developer's Note
+                                    </span>
+                                </div>
+                            </Link>
                         </div>
+
+                        <ul className='menu p-4 text-base-content'>
+                            <li>
+                                <Link href={ route('index') }>
+                                    <House size={16} />
+                                    <span>
+                                        Homepage
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={ route('dashboard') }>
+                                    <Gauge size={16} />
+                                    <span>
+                                        Dashboard
+                                    </span>
+                                </Link>
+                            </li>
+
+                            <li className='menu-title mt-4'>
+                                <span>
+                                    Data Utama
+                                </span>
+                            </li>
+                            <li>
+                                <Link>
+                                    <Tag size={16} />
+                                    <span>
+                                        Kategori
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link>
+                                    <Note size={16} />
+                                    <span>
+                                        Blog
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link>
+                                    <ChatTeardropText size={16} />
+                                    <span>
+                                        Komentar
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link>
+                                    <Users size={16} />
+                                    <span>
+                                        Pengguna
+                                    </span>
+                                </Link>
+                            </li>
+
+                            <li className='menu-title mt-4'>
+                                <span>
+                                    User Control
+                                </span>
+                            </li>
+                            <li>
+                                <Link>
+                                    <Gear size={16} />
+                                    <span>
+                                        Pengaturan
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    as='button'
+                                    className='hover:bg-red-500 hover:text-white'
+                                    href={ route('logout') }
+                                    method='POST'
+                                >
+                                    <SignOut size={16} />
+                                    <span>
+                                        Keluar
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-
-                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
-                    <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">{auth.user.name}</div>
-                            <div className="font-medium text-sm text-gray-500">{auth.user.email}</div>
-                        </div>
-
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            {header && (
-                <header className="bg-white shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
-                </header>
-            )}
-
-            <main>{children}</main>
-        </div>
+            </div>
+        </>
     );
 }
