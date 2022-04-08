9850fc60634d42a6fdbd5e24d50d42aef6fefde0
NikarashiHatsu <yourlovelydev@gmail.com>
2022-04-08 16:06:07 +0700

    chore: tailwind css aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
         require('daisyui'),
     ],
 };
