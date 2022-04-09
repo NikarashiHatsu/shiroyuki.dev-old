cd510b489009957b819be167dbee9745b386abbf
NikarashiHatsu <yourlovelydev@gmail.com>
2022-04-10 04:07:42 +0700

    chore: update filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,14 @@ return [
             'throw' => false,
         ],
 
+        'hosting' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public') . env('PATH_TO_PUBLIC_STORAGE'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
