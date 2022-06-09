70ac836b3af8ad16d73a168e5d24f177fa708356
NikarashiHatsu <yourlovelydev@gmail.com>
2022-06-10 02:41:40 +0700

    chore: fix cannot read property of null

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,11 +32,14 @@ document.querySelectorAll('pre code').forEach((block) => hljs.highlightElement(b
 // TUI Editor
 import '@toast-ui/editor/dist/toastui-editor.css'
 import Editor from '@toast-ui/editor'
-window.editor = new Editor({
-    initialValue: document.querySelector('#description').value,
-    el: document.querySelector('#editor'),
-    previewStyle: "vertical",
-    height: "600px",
-    initialEditType: "markdown",
-    useCommandShortcut: true,
-})
+
+if (document.querySelectorAll('#editor').length > 0) {
+    window.editor = new Editor({
+        initialValue: document.querySelector('#description').value,
+        el: document.querySelector('#editor'),
+        previewStyle: "vertical",
+        height: "600px",
+        initialEditType: "markdown",
+        useCommandShortcut: true,
+    })
+}
